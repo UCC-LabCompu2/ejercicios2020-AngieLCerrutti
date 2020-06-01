a4b271c234c77922bfa38bebe67e0eccf7c55edd
AngieLCerrutti <angiecerrutti@icloud.com>
2020-06-01 15:49:11 -0300

    se realizo ej de animacion

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -216,8 +216,24 @@ function dibujarImagen(posX, posY) {
     img.onload= function(){
         ctx.drawImage(img, posX, posY);
     }
+}
 
+x=0;
+dx=2;
+function animarAuto() {
+    var canvas= document.getElementById(elementId="myCanvas");
+    var ctx= canvas.getContext("2d");
 
+    canvas.width= canvas.width;
 
+    var img= new Image();
+    img.src= "images/auto.png";
 
+    img.onload= function(){
+        ctx.drawImage(img, x, dy=100);
+    }
+    if(x>canvas.width){
+        x=0;
+    }
+    x+=dx;
 }
